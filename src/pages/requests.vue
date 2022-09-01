db1425de5d6c1c782f6c77b57fc00d9fe3328216
KriseevM <mish.kryseev@gmail.com>
2022-09-02 01:58:11 +0300

    Add form validation

--- src/pages/requests.vue
+++ src/pages/requests.vue
@@ -4,10 +4,10 @@
   
     <b-form class="mt-4 mb-4 col-lg-2 container-fluid text-center" >
         <label>ID уровня</label>
-        <b-form-input class="mb-1 text-center" v-model="levelid" type="number" name="levelid" max="99999999" min="1" placeholder="12345678" id="levelid" required @input="formValueChanged" />
-        <b-button :variant="formValid ? 'success' : 'outline-danger'" id="submit" squared="false">Предложить</b-button>
+        <b-form-input class="mb-1 text-center" v-model="levelid" type="number" min="100" max="100000000" name="levelid" placeholder="12345678" id="levelid" required @input="formValueChanged" />
+        <b-button :variant="isValidating ? 'outline-disabled' : formValid ? 'success' : 'outline-danger'" id="submit" squared="false" @click="requestButtonClick">Предложить</b-button>
     </b-form>
-  
+
 </template>
 <script>
   export default {
@@ -17,19 +17,48 @@
         displayName: "Предложить уровень",
         index: 0,
         formValid: false,
-        levelid: ''
+        isValidating: false,
+        levelid: '',
       }
     },
     methods: {
-      formValueChanged()
+      async formValueChanged()
       {
+        this.isValidating = true
         this.$nextTick(async() => {
-          this.formValid = +this.levelid > 100 && +this.levelid < 100000000
+          if(+this.levelid < 100 || +this.levelid > 100000000)
+          {
+            this.formValid = false
+            this.isValidating = false
+            return;
+          }
+          let response = await fetch("/api/gdserver.CheckLevel?levelId=" + this.levelid, {
+            method: 'GET'
+          })
+          if(response.ok)
+          {
+            if(await response.json())
+            {
+              this.formValid = true
+              this.isValidating = false
+            }
+            else
+            {
+              this.formValid = false
+              this.isValidating = false
+            }
+          }
         });
 
+        
+      },
+      requestButtonClick()
+      {
+          
       }
     }
   }
+
 </script>
 <style>
 input[type=number]::-webkit-inner-spin-button, 
